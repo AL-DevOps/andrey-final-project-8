14aae19beb723c2244a40c09033189b03d92885c
AL-DevOps <96539143+AL-DevOps@users.noreply.github.com>
2022-12-10 23:06:04 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,6 @@
-output “cluster_id” {
-  description = “EKS cluster ID.”
-  value       = module.eks.cluster_id
+output "cluster_id" {
+    value = module.eks.cluster_id
 }
-
-output “cluster_endpoint” {
-  description = “Endpoint for EKS control plane.”
-  value       = module.eks.cluster_endpoint
+output "cluster_endpoint" {
+    value = module.eks.cluster_endpoint
 }
