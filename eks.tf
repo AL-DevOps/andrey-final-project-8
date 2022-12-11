91b98ee6ec67bb1af7b1895b38a263703efc87d0
AL-DevOps <96539143+AL-DevOps@users.noreply.github.com>
2022-12-11 11:34:53 +0200

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -15,7 +15,7 @@ worker_groups = [
         {
             name = "Worker-Group-1"
             instance_type = "t2.micro"
-            asg_desired_capacity = 2
+            asg_desired_capacity = 1
             additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
         },
         {
