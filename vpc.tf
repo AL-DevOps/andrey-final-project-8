a3c1ee35fa7ff26c1c747a338a6ad5ec00347a1b
AL-DevOps <96539143+AL-DevOps@users.noreply.github.com>
2022-12-20 18:07:59 +0200

    Update vpc.tf
    new 20.12.2022

--- vpc.tf
+++ vpc.tf
@@ -1,28 +1,26 @@
-variable "region" {
-    default = "eu-central-1"
-}
-data "aws_availability_zones" "available" {}
-locals {
-    cluster_name = "EKS-Cluster"
-}
-module vpc {
-    source = "terraform-aws-modules/vpc/aws"
-    version = "3.2.0"
-    name = "Andrey-VPC"
-    cidr = "10.0.0.0/16"
-    azs = data.aws_availability_zones.available.names
-    private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-    public_subnets =  ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-    enable_nat_gateway = true
-    single_nat_gateway = true
-    enable_dns_hostnames = true
-tags = {
-    "Name" = "Andrey-VPC"
-}
-public_subnet_tags = {
-    "Name" = "Andrey-Public-Subnet"
-}
-private_subnet_tags = {
-    "Name" = "Andrey-Private-Subnet"
-}
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.14.2"
+
+  name = "education-vpc"
+
+  cidr = "10.0.0.0/16"
+  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = 1
+  }
 }
